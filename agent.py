4f183bd284e9bd48ab82d7f9f77b97ffe08f61f9
Arkodeep Chatterjee <arkodeep3404@gmail.com>
2024-10-21 02:15:15 +0530

    update

--- agent.py
+++ agent.py
@@ -1,6 +1,5 @@
 from __future__ import annotations
 import logging
-import aiohttp
 from typing import Annotated
 from dotenv import load_dotenv
 import os
@@ -60,28 +59,6 @@ async def entrypoint(ctx: JobContext):
             "arkodeep3404@gmail.com",
         )
         return "emergency help email sent successfully"
-
-    # fnc_ctx = llm.FunctionContext()
-
-    # @fnc_ctx.ai_callable()
-    # async def get_weather(
-    #     location: Annotated[
-    #         str, llm.TypeInfo(description="The location to get the weather for")
-    #     ],
-    # ):
-    #     """Called when the user asks about the weather. This function will return the weather for the given location."""
-    #     logger.info(f"getting weather for {location}")
-    #     url = f"https://wttr.in/{location}?format=%C+%t"
-    #     async with aiohttp.ClientSession() as session:
-    #         async with session.get(url) as response:
-    #             if response.status == 200:
-    #                 weather_data = await response.text()
-    #                 # response from the function call is returned to the LLM
-    #                 return f"The weather in {location} is {weather_data}."
-    #             else:
-    #                 raise Exception(
-    #                     f"Failed to get weather data, status code: {response.status}"
-    #                 )
 
     await ctx.connect(auto_subscribe=AutoSubscribe.AUDIO_ONLY)
     participant = await ctx.wait_for_participant()
