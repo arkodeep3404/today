70db8c9570234efedcf20f4cfa1d6b3df3d54f9d
Arkodeep Chatterjee <arkodeep3404@gmail.com>
2024-10-20 23:50:23 +0530

    update

--- agent.py
+++ agent.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 import logging
-from typing import Annotated
 import aiohttp
+from typing import Annotated
 from dotenv import load_dotenv
 import os
 
@@ -19,6 +19,14 @@ from livekit.agents import (
 )
 from livekit.plugins import openai
 
+load_dotenv()
+logger = logging.getLogger("my-worker")
+logger.setLevel(logging.INFO)
+
+# print(os.getenv("LIVEKIT_URL"))
+# print(os.getenv("LIVEKIT_API_KEY"))
+# print(os.getenv("LIVEKIT_API_SECRET"))
+
 
 def send_email(subject, body, to_email):
     msg = EmailMessage()
@@ -30,42 +38,55 @@ def send_email(subject, body, to_email):
     with smtplib.SMTP_SSL(os.getenv("SMTP"), 465) as smtp:
         smtp.login(os.getenv("EMAIL"), os.getenv("PASSWORD"))
         smtp.send_message(msg)
-
-
-load_dotenv()
-logger = logging.getLogger("my-worker")
-logger.setLevel(logging.INFO)
-
-# print(os.getenv("LIVEKIT_URL"))
-# print(os.getenv("LIVEKIT_API_KEY"))
-# print(os.getenv("LIVEKIT_API_SECRET"))
-
-
+        
 async def entrypoint(ctx: JobContext):
     logger.info("starting entrypoint")
 
-    fnc_ctx = llm.FunctionContext()
+    class AssistantFnc(llm.FunctionContext):
 
-    @fnc_ctx.ai_callable()
-    def send_email(
-        location: Annotated[
-            str,
-            llm.TypeInfo(description="The location where to send help in emergency"),
-        ],
-    ):
-        """Called when user asks to send help email in emergency. This function will send help email in emergency."""
-        logger.info("sending help email in emergency")
+        @llm.ai_callable()
+        async def emergency_email(
+            location: Annotated[
+                str,
+                llm.TypeInfo(description="The location where to send help in emergency"),
+            ],
+        ):
+            """Called when user asks to send help email in emergency. This function will send help email in emergency."""
+            logger.info("sending help email in emergency")
 
-        send_email(
-            f"Urgent Help Needed at {location}",
-            f"This is an emergency situation at {location}. Immediate assistance is required. Please send help as soon as possible to address the critical situation.",
-            "arkodeep3404@gmail.com",
-        )
+            send_email(
+                f"Urgent Help Needed at {location}",
+                f"This is an emergency situation at {location}. Immediate assistance is required. Please send help as soon as possible to address the critical situation.",
+                "arkodeep3404@gmail.com",
+            )
 
-        return "emergency help email sent successfully"
+            return "emergency help email sent successfully"
+    
+    # fnc_ctx = llm.FunctionContext()
+
+    # @fnc_ctx.ai_callable()
+    # async def get_weather(
+    #     location: Annotated[
+    #         str, llm.TypeInfo(description="The location to get the weather for")
+    #     ],
+    # ):
+    #     """Called when the user asks about the weather. This function will return the weather for the given location."""
+    #     logger.info(f"getting weather for {location}")
+    #     url = f"https://wttr.in/{location}?format=%C+%t"
+    #     async with aiohttp.ClientSession() as session:
+    #         async with session.get(url) as response:
+    #             if response.status == 200:
+    #                 weather_data = await response.text()
+    #                 # response from the function call is returned to the LLM
+    #                 return f"The weather in {location} is {weather_data}."
+    #             else:
+    #                 raise Exception(
+    #                     f"Failed to get weather data, status code: {response.status}"
+    #                 )
 
     await ctx.connect(auto_subscribe=AutoSubscribe.AUDIO_ONLY)
     participant = await ctx.wait_for_participant()
+    fnc_ctx = AssistantFnc()
 
     agent = multimodal.MultimodalAgent(
         model=openai.realtime.RealtimeModel.with_azure(
